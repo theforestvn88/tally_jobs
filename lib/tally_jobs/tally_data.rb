422d0f2f081fcd0ffbd9f3142f41958d7dcf3e25
theforestvn88 <theforestvn88@gmail.com>
2024-04-20 21:07:11 +0700

    fix tally-data mix together on many jobs class

--- lib/tally_jobs/tally_data.rb
+++ lib/tally_jobs/tally_data.rb
@@ -5,14 +5,15 @@ module TallyJobs
         def self.included(base)
             base.extend ClassMethods
             base.class_eval do
+                cattr_reader :data_for_all_tasks, default: []
+                cattr_reader :data_for_each_tasks, default: []
+
                 def self.data_for_all(block)
-                    @@data_for_all_tasks ||= []
-                    @@data_for_all_tasks << block
+                    self.data_for_all_tasks << block
                 end
             
                 def self.data_for_each(block)
-                    @@data_for_each_tasks ||= []
-                    @@data_for_each_tasks << block
+                    self.data_for_each_tasks << block
                 end
             end
         end
@@ -27,9 +28,8 @@ module TallyJobs
         end
     
         def perform(*args)
-            p *args
-            data_for_all = @@data_for_all_tasks.map { |q| q.call(*args) }
-            datas = @@data_for_each_tasks.map { |q| q.call(*args) }.inject(&:zip)
+            data_for_all = data_for_all_tasks.map { |q| q.call(*args) }
+            datas = data_for_each_tasks.map { |q| q.call(*args) }.inject(&:zip)
             datas.each { |data| each_do(*data_for_all, *data) }
         end
     end
--- spec/tally_data_spec.rb
+++ spec/tally_data_spec.rb
@@ -15,25 +15,55 @@ RSpec.describe TallyJobs::TallyData do
         end
     end
 
+    class AnotherJob
+        include TallyJobs::TallyData
+
+        data_for_all lambda { |params| -100 }
+        data_for_each lambda { |params| params.map { |p| p.class } }
+        data_for_each lambda { |params| params.map { |p| p } }
+
+        def each_do(data_all, data_each_1, data_each_2)
+        end
+
+        def self.perform_later(args)
+        end
+    end
+
     it "enqueue to tally" do
+        TallyJobs::JOBS_QUEUE.clear
+
         AJob.enqueue_to_tally(1)
         AJob.enqueue_to_tally(2, "second")
         AJob.enqueue_to_tally(3, [4,5,6])
+        AnotherJob.enqueue_to_tally(8)
+        AnotherJob.enqueue_to_tally(:x)
 
-        expect(TallyJobs::JOBS_QUEUE.deq).to eq([AJob, [1]])
-        expect(TallyJobs::JOBS_QUEUE.deq).to eq([AJob, [2, "second"]])
-        expect(TallyJobs::JOBS_QUEUE.deq).to eq([AJob, [3, [4,5,6]]])
+        expect(TallyJobs::JOBS_QUEUE.deq).to eq([AJob, 1])
+        expect(TallyJobs::JOBS_QUEUE.deq).to eq([AJob, 2, "second"])
+        expect(TallyJobs::JOBS_QUEUE.deq).to eq([AJob, 3, [4,5,6]])
+        expect(TallyJobs::JOBS_QUEUE.deq).to eq([AnotherJob, 8])
+        expect(TallyJobs::JOBS_QUEUE.deq).to eq([AnotherJob, :x])
     end
 
     it "should perform with prepared data" do
         called_params = []
-        allow_any_instance_of(AJob).to receive(:each_do) do |_, *args|
-            called_params << args
+
+        allow_any_instance_of(AJob).to receive(:each_do) do |job, *args|
+            called_params << [job.class, *args]
         end
 
-        job = AJob.new
-        job.perform([1, [2, 3], :x])
+        allow_any_instance_of(AnotherJob).to receive(:each_do) do |job, *args|
+            called_params << [job.class, *args]
+        end
 
-        expect(called_params).to eq([[100, "Integer", "1"], [100, "Array", "[2, 3]"], [100, "Symbol", "x"]])
+        TallyJobs::JOBS_QUEUE.clear
+
+        AJob.new.perform([1, [2, 3], :x])
+        AnotherJob.new.perform([1, [2, 3], :x])
+
+        expect(called_params).to eq([
+            [AJob, 100, "Integer", "1"], [AJob, 100, "Array", "[2, 3]"], [AJob, 100, "Symbol", "x"],
+            [AnotherJob, -100, Integer, 1], [AnotherJob, -100, Array, [2, 3]], [AnotherJob, -100, Symbol, :x]
+        ])
     end
 end
